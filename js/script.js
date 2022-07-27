86f239b1dd7e94fdecfac81f5ce68f00186349e6
Florencia Avigliano <floravigliano@gmail.com>
2022-07-27 01:23:49 -0300

    agregar descuentos

--- js/script.js
+++ js/script.js
@@ -25,21 +25,21 @@ if (ingresar) {
     //"Accedemos" a la info de la cuenta
 
 class descuentos {
-    grupoDeAliades(id, Aliade,tipoDeDescuento,montoDeDescuento){
-        this.id = id;
+    grupoDeAliades(Aliade,tipoDeDescuento,montoDeDescuento){
+        
         this.Aliade = Aliade;
         this.tipoDeDescuento = tipoDeDescuento;
         this.montoDeDescuento = montoDeDescuento;
     }
 }
 
-const gastronomia = [{ id: 100, Aliade: 'casabrandon', tipoDeDescuento: 'gastronomia',montoDeDescuento:'10%'}, { id: 100, Aliade: 'lo de Nelly ', tipoDeDescuento: 'gastronomia',montoDeDescuento:'20%'}];
-const ropa=[{ id: 200, Aliade: 'serigrafiaUrgente', tipoDeDescuento: 'ropa', montoDeDescuento:'20%'}, { id: 200, Aliade: 'Wipp', tipoDeDescuento: 'ropa', montoDeDescuento:'10%'}];
-const accesorios= [{ id: 300, Aliade: 'latirolesa', tipoDeDescuento: 'accesorios',montoDeDescuento:'10%' }, { id: 300, Aliade: 'miramelasmedias', tipoDeDescuento: 'accesorios',montoDeDescuento:'15%' }];
-const libros= [{ id: 400, Aliade: 'Cespedes', tipoDeDescuento: 'libros',montoDeDescuento:'15%'},{ id: 400, Aliade: 'KOKORO', tipoDeDescuento: 'libros',montoDeDescuento:'10%'}];
+const gastronomia = [{Aliade: 'casabrandon', tipoDeDescuento: 'gastronomia',montoDeDescuento:'10%'}, { id: 100, Aliade: 'lo de Nelly ', tipoDeDescuento: 'gastronomia',montoDeDescuento:'20%'}];
+const ropa=[{ Aliade: 'Serigrafia Urgente', tipoDeDescuento: 'ropa', montoDeDescuento:'20%'}, { id: 200, Aliade: 'Wipp', tipoDeDescuento: 'ropa', montoDeDescuento:'10%'}];
+const accesorios= [{ Aliade: 'Latirolesa', tipoDeDescuento: 'accesorios',montoDeDescuento:'10%' }, { id: 300, Aliade: 'mirame las Medias', tipoDeDescuento: 'accesorios',montoDeDescuento:'15%' }];
+const libros= [{ Aliade: 'Cespedes', tipoDeDescuento: 'libros',montoDeDescuento:'15%'},{ id: 400, Aliade: 'KOKORO', tipoDeDescuento: 'libros',montoDeDescuento:'10%'}];
 
 
-const descuentosClub  = [gastronomia, ropa, accesorios, libros]
+// const descuentosClub  = [gastronomia, ropa, accesorios, libros]
 
 let elegir= parseInt (prompt("Elegi que clase de descuento estas buscando: \n 1- Gastronomia  \n 2- Ropa \n 3- Accesorios \n 4 - libros"));
 
@@ -49,7 +49,7 @@ switch(elegir) {
             alert(i.Aliade + " "+ i.tipoDeDescuento + " "+ i.montoDeDescuento+ " ")
         };
     case 2: for (i of ropa){
-        alert(i.Aliade + " "+ i.tipoDeDescuento + " "+ i.montoDeDescuento+ " ")
+        alert(i.Aliade+ " "+ i.tipoDeDescuento + " "+ i.montoDeDescuento+ " ")
     };
     case 3: 
     for (i of accesorios){
@@ -58,8 +58,22 @@ switch(elegir) {
     case 4: for (i of libros){
         alert(i.Aliade + " "+ i.tipoDeDescuento + " "+ i.montoDeDescuento+ " ")
     };
+    case 5: break;
+    default: break;
+}
 
-    console.log (descuentosClub);
-    let elegir= parseInt (prompt ("Ver mas descuentos disponibles \n 1- Gastronomia  \n 2- Ropa \n 3- Accesorios \n 4 - libros"));}
+console.log(descuentos)
+alert("Queres ofrecer un beneficio?")
+let Aliade = prompt ('Nombre de tu emprendimiento')
+let tipoDeDescuento = prompt (' que tipo de emprendimiento tenes? \n 1- Gastronomia  \n 2- Ropa \n 3- Accesorios \n 4 - libros')
+let montoDeDescuento = prompt ('que descuento te gustaria ofrecer? \n 1- 10%, \n 2-15%, \n 3- 20%')
+
+let grupoDeAliades=( Aliade, tipoDeDescuento, montoDeDescuento );
+descuentos.push
+
+alert ('Gracias por elegirnos');
+
+// console.log (descuentosClub);
+// let volver= parseInt (prompt ("Ver mas descuentos disponibles \n 1- Gastronomia  \n 2- Ropa \n 3- Accesorios \n 4 - libros"));}
 
 }
